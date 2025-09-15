ae81942cc7f0ccfb85319254b30325f8c06bdc0c
Ika Mania <maniaika07@gmail.com>
2025-09-15 04:00:04 +0400

    refactor(): update InputField components to use value and onChange

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,15 @@
 import Authform from "./auth/Authform"
 import Inputfield from "./auth/Inputfield.jsx"
 
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const fields = [
-    <Inputfield label="Email:" placeholder="Enter your email" type="email" />,
-    <Inputfield label="Password:" placeholder="Enter your password" type="text" />,
+    <Inputfield value={email} onChange={(e) => setEmail(e.target.value)} label="Email:" placeholder="Enter your email" type="email" />,
+    <Inputfield value={password} onChange={(e) => setPassword(e.target.value)} label="Password:" placeholder="Enter your password" type="text" />,
   ]
 
   return (
